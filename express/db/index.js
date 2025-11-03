677557faaa530873f6c0548b381ede965ebaa944
Yaroslav <katsarskyiy@gmail.com>
2025-11-03 15:30:49 +0200

    railway

--- express/db/index.js
+++ express/db/index.js
@@ -7,4 +7,10 @@ export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
--- express/server.js
+++ express/server.js
@@ -42,6 +42,6 @@ app.delete("/todos/:id", async (req, res) => {
   res.status(200).json(await todoService.getAll());
 });
 
-app.listen(3000, () =>
-  console.log("Server is running on http://localhost:3000/")
-);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
